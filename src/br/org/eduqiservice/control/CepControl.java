963ca54c3d815344ba6eb1d01246f033e3c2b397
Samuel Zvir <samuelyuri@gmail.com>
2013-04-14 09:53:37 -0300

    CEP adicionado

--- src/br/org/eduqiservice/control/CepControl.java
+++ src/br/org/eduqiservice/control/CepControl.java
@@ -1,12 +1,17 @@
 package br.org.eduqiservice.control;
 
 import br.org.eduqiservice.dao.CepEscolaDAOImpl;
+import br.org.eduqiservice.model.CepEscola;
 
 public class CepControl {
 
 	
 	public static String getCEP(int idEscola){
 		CepEscolaDAOImpl dao = new CepEscolaDAOImpl();
-		return dao.findById(idEscola).getNumCep();
+		 CepEscola cep = dao.findById(idEscola);
+		 if(cep == null){
+			 return "";
+		 }
+		 return cep.getNumCep();
 	}
 }
