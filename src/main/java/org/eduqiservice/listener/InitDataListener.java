b920bc65a9847389eabf67ba4b48fa4433682fab
Samuel Zvir <samuelyuri@gmail.com>
2014-07-12 19:37:25 -0300

    delete unused classes.

--- src/main/java/org/eduqiservice/listener/InitDataListener.java
+++ src/main/java/org/eduqiservice/listener/InitDataListener.java
@@ -7,10 +7,8 @@ import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 import org.eduqiservice.control.CacheSearchController;
 import org.eduqiservice.core.util.EduqiPropertiesLoader;
-import org.eduqiservice.log.LogUtils;
 import org.eduqiservice.thread.ProbThread;
 
 /**
@@ -30,11 +28,7 @@ public class InitDataListener implements ServletContextListener, HttpSessionList
         
     	ServletContext context = contextEvent.getServletContext();
    
-    	//configura log4j
-    	String logpath = context.getRealPath("WEB-INF/log4j/log4j.properties");
-		LogUtils.setPathLog(logpath);
-		PropertyConfigurator.configure(logpath);
-    	
+ 	
 		//configura propriedades utilizadas
 		String props = context.getRealPath("WEB-INF/properties/properties.properties");
     	EduqiPropertiesLoader.setPropertiesPath(props);
